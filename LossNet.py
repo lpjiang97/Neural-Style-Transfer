fedab49e6d819a4a54ee1901265c6c8639fa0e6f
lpjiang97 <prestonj@cs.washington.edu>
2018-06-04 16:31:15 -0700

    lossnet dev

--- LossNet.py
+++ LossNet.py
@@ -4,6 +4,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.models as models
 import torchvision.transforms as transforms
+import torch.utils.data
+import torchvision.datasets as datasets
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -12,6 +14,11 @@ from util import *
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+imsize = 256
+loader = transforms.Compose([
+             transforms.Scale(imsize),
+             transforms.ToTensor()
+         ])
 
 
 def gram(x):
@@ -65,11 +72,9 @@ class LossNet():
         self.content = content
         # style image
         self.style = style
-        # final output to be saved
-        self.x = self.content.clone()
         # where to insert loss
-        self.content_layer = ['conv_4']
-        self.style_layer = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+        self.content_layers = ['conv_4']
+        self.style_layers = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
         # weight to update style and content, should be huge on style, little on content
         self.style_weight = style_weight
         self.content_weight = content_weight
@@ -97,110 +102,133 @@ class LossNet():
         ).to(device)
         # optimizer
         self.optimizer = optim.Adam(self.trans_net.parameters(), lr=1e-3)
-        # start with norm layer 
-        self.model = nn.Sequential(Normalization(
-            torch.tensor([0.485, 0.456, 0.406]).to(device), 
-            torch.tensor([0.229, 0.224, 0.225]).to(device)))
-    
-    def build_model(self):
-        content_losses = []
-        style_losses = []
-
-        content = self.content.clone()
-        style = self.style.clone()
+        # gram matrix loss
+        self.style_loss = StyleLoss()
         
-        i = 0  # increment every time we see a conv
-        for layer in self.vgg.children():
-            if isinstance(layer, nn.Conv2d):
-                i += 1
-                name = 'conv_{}'.format(i)
-            elif isinstance(layer, nn.ReLU):
-                name = 'relu_{}'.format(i)
-                layer = nn.ReLU(inplace=False)
-            elif isinstance(layer, nn.MaxPool2d):
-                name = 'pool_{}'.format(i)
-            elif isinstance(layer, nn.BatchNorm2d):
-                name = 'bn_{}'.format(i)
-            else:
-                raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
+    
+    # def build_model(self):
+    #     content_losses = []
+    #     style_losses = []
 
-            self.model.add_module(name, layer)
+    #     content = self.content.clone()
+    #     style = self.style.clone()
+    #     
+    #     i = 0  # increment every time we see a conv
+    #     for layer in self.vgg.children():
+    #         if isinstance(layer, nn.Conv2d):
+    #             i += 1
+    #             name = 'conv_{}'.format(i)
+    #         elif isinstance(layer, nn.ReLU):
+    #             name = 'relu_{}'.format(i)
+    #             layer = nn.ReLU(inplace=False)
+    #         elif isinstance(layer, nn.MaxPool2d):
+    #             name = 'pool_{}'.format(i)
+    #         elif isinstance(layer, nn.BatchNorm2d):
+    #             name = 'bn_{}'.format(i)
+    #         else:
+    #             raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
 
-            if name in self.content_layer:
-                # add content loss:
-                target = self.model(content).detach()
-                content_loss = ContentLoss(target)
-                self.model.add_module("content_loss_{}".format(i), content_loss)
-                content_losses.append(content_loss)
+    #         self.model.add_module(name, layer)
 
-            if name in self.style_layer:
-                # add style loss:
-                target_feature = self.model(style).detach()
-                style_loss = StyleLoss(target_feature)
-                self.model.add_module("style_loss_{}".format(i), style_loss)
-                style_losses.append(style_loss)
+    #         if name in self.content_layer:
+    #             # add content loss:
+    #             target = self.model(content).detach()
+    #             content_loss = ContentLoss(target)
+    #             self.model.add_module("content_loss_{}".format(i), content_loss)
+    #             content_losses.append(content_loss)
 
-        # trimming
-        for i in range(len(self.model) - 1, -1, -1):
-            if isinstance(self.model[i], ContentLoss) or isinstance(self.model[i], StyleLoss):
-                break
+    #         if name in self.style_layer:
+    #             # add style loss:
+    #             target_feature = self.model(style).detach()
+    #             style_loss = StyleLoss(target_feature)
+    #             self.model.add_module("style_loss_{}".format(i), style_loss)
+    #             style_losses.append(style_loss)
 
-        self.model = self.model[:(i + 1)]
-        return style_losses, content_losses
+    #     # trimming
+    #     for i in range(len(self.model) - 1, -1, -1):
+    #         if isinstance(self.model[i], ContentLoss) or isinstance(self.model[i], StyleLoss):
+    #             break
+
+    #     self.model = self.model[:(i + 1)]
+    #     return style_losses, content_losses
     
     def train(self, content):
-        print('Start model...')
-        style_losses, content_losses = self.build_model()
+        self.optimizer.zero_grad()
 
-        run = [0]
-        while run[0] <= epochs:
-            # we use Adam now, now need for closure 
-            
-            def closure():
-                # correct the values of updated input image
-                self.x.data.clamp_(0, 1)
+        content = content.clone()
+        style = self.style.clone()
+        x = self.transformation_network.forward(content)
 
-                self.optimizer.zero_grad()
-                self.x = self.trans_net(self.x)
-                self.model(self.x)
-                style_score = 0
-                content_score = 0
+        content_loss = 0
+        style_loss = 0
 
-                for sl in style_losses:
-                    style_score += sl.loss
-                for cl in content_losses:
-                    content_score += cl.loss
+        i = 1
+        not_inplace = lambda layer: nn.ReLU(inplace=False) if isinstance(layer, nn.ReLU) else layer
+        for layer in list(self.vgg.features):
+            layer = not_inplace(layer).to(device)
 
-                style_score *= self.style_weight
-                content_score *= self.content_weight
+            x, content, style = layer.forward(x), layer.forward(content), layer.forward(style)
 
-                loss = style_score + content_score
-                loss.backward()
+            if isinstance(layer, nn.Conv2d):
+                name = "conv_" + str(i)
 
-                run[0] += 1
-                if run[0] % 50 == 0:
-                    print("run {}:".format(run))
-                    print('Style Loss : {:4f} Content Loss: {:4f}'.format(
-                        style_score.item(), content_score.item()))
-                    print()
+                if name in self.content_layers:
+                    content_loss += self.loss(x * self.content_weight, content.detach() * self.content_weight)
+                if name in self.style_layers:
+                    x_grad, style_grad = self.style_loss.forward(x), self.style_loss.forward(style)
+                    style_loss += self.loss(x_grad * self.style_weight, style_grad.detach() * self.style_weight)
 
-                return style_score + content_score
+            if isinstance(layer, nn.ReLU):
+                i += 1
 
-            closura
-            self.optimizer.step()
-        # a last correction...
-        self.x.data.clamp_(0, 1)
+        total_loss = content_loss + style_loss
+        total_loss.backward()
 
+
+def save_images(input, paths):
+    N = input.size()[0]
+    images = input.data.clone().cpu()
+    for n in range(N):
+        image = images[n]
+        image = image.view(3, imsize, imsize)
+        image = unloader(image)
+        scipy.misc.imsave(paths[n], image)
+ 
 
 def main():
+    N = 4
+    num_epochs = 3
+
     style_img = image_loader("images/starry.jpg", device)
     content_img = image_loader("images/max.jpg", device)
 
     assert style_img.size() == content_img.size(), \
         "we need to import style and content images of the same size"
+
+    coco = datasets.ImageFolder(root='data/contents', transform=loader)
+    content_loader = torch.utils.data.DataLoader(coco, batch_size=N, shuffle=True, **kwargs)
+
     lossnet = LossNet(content_img, style_img)
-    lossnet.train(300)
-    image_saver(lossnet.x, "test.png")
+
+    for epoch in range(num_epochs):
+        for i, content_batch in enumerate(content_loader):
+          iteration = epoch * i + i
+          content_loss, style_loss, x = lossnet.train(content_batch, style_batch)
+
+          if i % 10 == 0:
+              print("Iteration: %d" % (iteration))
+              print("Content loss: %f" % (content_loss.data[0]))
+              print("Style loss: %f" % (style_loss.data[0]))
+
+          if i % 500 == 0:
+              path = "outputs/%d_" % (iteration)
+              paths = [path + str(n) + ".png" for n in range(N)]
+              save_images(x, paths)
+
+              path = "outputs/content_%d_" % (iteration)
+              paths = [path + str(n) + ".png" for n in range(N)]
+              save_images(content_batch, paths)
+              lossnet.save()
 
 
 if __name__ == "__main__":
